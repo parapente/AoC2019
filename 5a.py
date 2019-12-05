d42aafb87dcfbcfc8db6b241a7bc4355b80a0e32
Theofilos Intzoglou <int.teo@gmail.com>
2019-12-05 22:02:13 +0200

    Day 5, Python completed

---
+++ 5a.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import sys
+
+
+with open('5.dat') as f:
+    lines = f.read()
+lines = lines.split("\n")
+lines.pop()
+data = lines[0].split(",")
+data = [int(x) for x in data]
+
+ip = 0
+while ip >= 0:
+    op = data[ip]
+    # print(op)
+    mode = [0] * 3
+    for i in range(3):
+        mode[2 - i] = op // pow(10, 4 - i)
+        op %= pow(10, 4 - i)
+
+    # Check ops
+    if op == 1:
+        parm1 = data[ip+1] if mode[0] else data[data[ip+1]]
+        parm2 = data[ip+2] if mode[1] else data[data[ip+2]]
+        # print('Add ', parm1, parm2)
+        sub = parm1 + parm2
+        data[data[ip+3]] = sub
+        ip += 4
+    elif op == 2:
+        parm1 = data[ip+1] if mode[0] else data[data[ip+1]]
+        parm2 = data[ip+2] if mode[1] else data[data[ip+2]]
+        # print('Multiply ', parm1, parm2)
+        sub = parm1 * parm2
+        data[data[ip+3]] = sub
+        ip += 4
+    elif op == 3:
+        val = int(input('Input: '))
+        parm = data[ip+1]
+        data[parm] = val
+        ip += 2
+    elif op == 4:
+        parm = data[ip+1] if mode[0] else data[data[ip+1]]
+        print('Output:', parm)
+        ip += 2
+    elif op == 99:
+        ip = -1
+    else:
+        print('Error! Found op value: ', data[ip])
+        sys.exit(1)
+print('Done!')
